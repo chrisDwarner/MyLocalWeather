4702dead1db77471c759df1caef22301598d5566
Chris Warner <chris@ccwdevstudio.com>
2022-01-28 08:58:22 -0800

    Initial Commit

---
+++ MyLocalWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyLocalWeather
+//
+//  Created by chris warner on 1/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyLocalWeather/MyLocalWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLocalWeatherApp.swift
+//  MyLocalWeather
+//
+//  Created by chris warner on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct MyLocalWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
