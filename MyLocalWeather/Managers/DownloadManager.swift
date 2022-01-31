bb9ca4a4ca81a61607241102ac412ed7925c26e4
Chris Warner <chris@ccwdevstudio.com>
2022-01-30 20:40:35 -0800

    downloading OneCall api results

--- MyLocalWeather/Managers/DownloadManager.swift
+++ MyLocalWeather/Managers/DownloadManager.swift
@@ -9,42 +9,42 @@ import Foundation
 import Combine
 import SwiftUI
 
-class DownloadManager {
-    static var shared: DownloadManager { DownloadManager() }
+class DownloadManager: ObservableObject {
+//    static var shared: DownloadManager { DownloadManager() }
     
     // openWeather.org apiKey
     fileprivate let apiKey = "84431f2f4742d2281cb6c91f46e2074e"
-    var subscriber: Cancellable? = nil
-    
-    private init() {}
+
+//    private init() {}
     
     func fetchOneCall(for city: City, block: @escaping (OneCall?, Error?)->Void ) {
         guard let location = city.location?.queryString,
-              let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?appId=\(apiKey)\(location)") else { return }
+              let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?appId=\(apiKey)\(location)") else {
+                  block(nil, APIError.apiError(reason: "bad URL request"))
+                  return
+              }
 
         let publisher = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if let err = error {
-                print(err)
+            if let _ = error {
+                block( nil, error)
                 return
             }
+            
             guard let status = (response as? HTTPURLResponse)?.statusCode, 200..<299 ~= status else {
+                block(nil, APIError.apiError(reason: "bad HTTP response"))
                 return
             }
             guard let data = data else { return }
             do {
-                print( String(decoding: data, as: UTF8.self))
                 let values = try JSONDecoder().decode(OneCall.self, from: data)
-                print(values)
+                block( values, nil )
             }
             catch {
-                print(error)
+                block( nil, error)
             }
-
         }
         publisher.resume()
     }
-    
-    
 }
 
 extension Location {
